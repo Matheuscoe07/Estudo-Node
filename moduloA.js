4575b7b0708893955ae194ee5cb7c4899ba6e5ac
Matheuscoe07 <100366370+Matheuscoe07@users.noreply.github.com>
2023-08-07 14:24:11 -0300

    Modulo em Node

---
+++ moduloA.js
@@ -0,0 +1,13 @@
+this.ola = 'Fala Pessoal'
+exports.bemVindo = 'Bem vindo ao Node!'
+module.exports.ateLogo = 'Até mais!'
+
+/*
+- o conteudo de um módulo/arquivo só é exposto para
+outros módulos/arquivos caso seja feita a exportação
+- enquanto não fizer a exportação, o conteudo
+só esta disponivel localmente (dentro do proprio
+    modulo ou arquivo)
+- as três linhas acima mostram as três formas diferentes
+de se exportar um módulo
+*/
