1addfad716baef2c3589ca149facb776315d8c78
Matheuscoe07 <100366370+Matheuscoe07@users.noreply.github.com>
2023-08-21 13:39:00 -0300

    API com Express #2

--- projeto/src/servidor.js
+++ projeto/src/servidor.js
@@ -2,6 +2,7 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 /*
 app.get('/produtos', (req, res, next) => {
@@ -11,7 +12,19 @@ app.get('/produtos', (req, res, next) => {
 */
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 123.45 }) //esse objeto sera convertido para JSON
+    res.send(bancoDeDados.getProdutos()) 
+})
+
+app.get('./produtos/:id', (rec, res, next) => { //o : indica que acessara o id
+    res.send(bancoDeDados.getProduto(req.params.id)) 
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta), () => {
